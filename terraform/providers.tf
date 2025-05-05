a6643909941824f34389740217f01a349fef113f
adrianlopezlanchares <202107370@alu.comillas.edu>
2025-05-05 08:17:39 +0200

    gitignore

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("../../icai2025-5f9879654f17.json")
+  credentials = file("/Users/adrian/Documents/ICAI/4o/2o Cuatri/Digitalizacion/ExamenFinal/TD-exam-final-alumni/examen-final-adrian-3017197d6fec.json")
 
   project = var.gcp-project
   region  = var.gcp-region
